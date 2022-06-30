11e6d4bab669f7798fcde4c0ce8715de41008570
sandeepksd <sandeepksd@gmail.com>
2022-06-30 06:15:27 +0530

    Initial code

---
+++ src/test/java/com/digiassist/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digiassist.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
